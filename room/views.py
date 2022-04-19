edb9193c828ccdf4886920da13e112f6b8f78c0f
KRAKOU Danho Guy Sylvestre <guysylvestre876@gmail.com>
2022-04-19 11:52:20 +0000

    Show list of room

--- room/views.py
+++ room/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import Room
 
-# Create your views here.
+@login_required
+def rooms(request):
+    rooms = Room.objects.all()
+    context = {
+        "rooms": rooms
+    }
+    return render(request, 'room/rooms.html', context)
