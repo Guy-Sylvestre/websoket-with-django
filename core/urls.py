0f63ca3ace971071cf4e06f855422fe1851a5ccf
KRAKOU Danho Guy Sylvestre <guysylvestre876@gmail.com>
2022-04-19 09:21:48 +0000

    Log out functionality

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.frontalPage, name='frontalpage'),
     path('signup/', views.signUp, name='signup'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
