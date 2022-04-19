142b57c3fc4e0034df3a7bb9d02736841eb730f6
KRAKOU Danho Guy Sylvestre <guysylvestre876@gmail.com>
2022-04-19 09:37:35 +0000

    Create app app for rooms

--- core/urls.py
+++ core/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.frontalPage, name='frontalpage'),
     path('signup/', views.signUp, name='signup'),
+    path('login/',  auth_views.LoginView.as_view(template_name="core/login.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
--- djangochat/settings.py
+++ djangochat/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     "channels",
     "core.apps.CoreConfig",
+    "room.apps.RoomConfig",
 ]
 
 MIDDLEWARE = [
